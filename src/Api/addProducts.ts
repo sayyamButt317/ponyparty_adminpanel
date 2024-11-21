0748ad709ecc1a550ec378bdfdfcfaa2f2cd7848
Sayyam <sayyambutt317@gmail.com>
2024-11-20 16:57:52 -0800

    write add product api

--- src/Api/addProducts.ts
+++ src/Api/addProducts.ts
@@ -1,8 +1,22 @@
 import dbConnect from "@/db/dbConnect";
+import {product} from "@/Model/product.model";
+import { NextResponse } from "next/server";
 
-export async function POST(request:Request) {
-    await dbConnect();
+export async function POST(request,{ params }) {
 
-    
+await dbConnect();
+const {id} = params;
+const {Description,ProductName,ActualPrice,DiscountedPrice,ProductInStock,Category} = await request.json();
+try {
+    await product.findByIdandpdate(id,{Description,ProductName,ActualPrice,DiscountedPrice,ProductInStock,Category})
+return NextResponse.json({message: 'Product updated successfully'},{status:200})
+} catch (error) {
+    console.log("Error in uploading Product details", error)
+    return Response.json({
+        success: false,
+        message: "Unauthorized"
+    }, { status:  401})
+}
+
 
 }
